38115c43195550303ae93ce6c7907c6edcb23a3a
fabito <fuechi@ciandt.com>
2016-12-21 20:17:04 -0200

    sample app using metric exporter

---
+++ samples/metrics/src/main/java/io/github/fabito/dropwizard/samples/SampleConfiguration.java
@@ -0,0 +1,14 @@
+package io.github.fabito.dropwizard.samples;
+
+import io.dropwizard.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SampleConfiguration extends Configuration {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SampleConfiguration.class);
+
+}
+
+
+
---
+++ samples/metrics/src/main/java/io/github/fabito/dropwizard/samples/resources/EchoResource.java
@@ -0,0 +1,35 @@
+package io.github.fabito.dropwizard.samples.resources;
+
+import com.codahale.metrics.annotation.ExceptionMetered;
+import com.codahale.metrics.annotation.Metered;
+import com.codahale.metrics.annotation.Timed;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by fabio on 07/12/16.
+ */
+@Path("/api")
+@Metered
+@Timed
+@ExceptionMetered
+public class EchoResource {
+
+    @GET
+    @Path("/echo")
+    public Response echo(@QueryParam("echo") String echo) {
+        return Response.ok(echo).build();
+    }
+
+    @GET
+    @Path("/badecho")
+    public Response badecho(@QueryParam("echo") String echo) {
+        return Response.serverError().build();
+    }
+
+
+
+}
