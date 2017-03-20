a099489202ae608f1e75eeccce77954217054e00
fabito <fuechi@ciandt.com>
2017-03-20 08:30:36 -0300

    added more logging samples

--- samples/logging/src/main/java/io/github/fabito/dropwizard/samples/SampleConfiguration.java
+++ samples/logging/src/main/java/io/github/fabito/dropwizard/samples/SampleConfiguration.java
@@ -1,12 +1,9 @@
 package io.github.fabito.dropwizard.samples;
 
 import io.dropwizard.Configuration;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class SampleConfiguration extends Configuration {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(SampleConfiguration.class);
 
 }
 
--- samples/logging/src/main/java/io/github/fabito/dropwizard/samples/resources/EchoResource.java
+++ samples/logging/src/main/java/io/github/fabito/dropwizard/samples/resources/EchoResource.java
@@ -1,5 +1,9 @@
 package io.github.fabito.dropwizard.samples.resources;
 
+import com.google.common.base.Charsets;
+import com.google.common.base.Preconditions;
+import com.google.common.io.CharStreams;
+import com.google.common.io.Closeables;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,6 +11,9 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * Created by fabio on 07/12/16.
@@ -16,14 +23,8 @@ public class EchoResource {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(EchoResource.class);
 
-    private static final String MULTILINE = "\n" +
-            "   __    _____  ___   ___  ____  _  _  ___\n" +
-            "  (  )  (  _  )/ __) / __)(_  _)( \\( )/ __)\n" +
-            "   )(__  )(_)(( (_-.( (_-. _)(_  )  (( (_-.\n" +
-            "  (____)(_____)\\___/ \\___/(____)(_)\\_)\\___/";
-
     @GET
-    public Response echo(@QueryParam("msg") String echo) {
+    public Response echo(@QueryParam("msg") String echo) throws IOException {
 
         LOGGER.trace(echo);
         LOGGER.debug(echo);
@@ -31,12 +32,15 @@ public class EchoResource {
         LOGGER.warn(echo);
 
         try {
-            throw new IllegalArgumentException("Illegal message!!");
-        } catch (IllegalArgumentException t) {
+            Preconditions.checkState(echo.equals("ha"));
+        } catch (IllegalStateException t) {
             LOGGER.error("Error while echoing.", t);
         }
 
-        LOGGER.info(MULTILINE);
+        InputStream is = this.getClass().getResourceAsStream("/banner.txt");
+        String multiline = CharStreams.toString(new InputStreamReader(is, Charsets.UTF_8));
+        LOGGER.info(multiline);
+        Closeables.closeQuietly(is);
 
         return Response.ok(echo).build();
     }
